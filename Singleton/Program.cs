194dac8779fe42886002fab42bd3959b9164080d
ugurkryl41 <ugurkarayel41@gmail.com>
2021-02-06 23:56:26 +0300

    Proje dosyası ekle.

---
+++ Singleton/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customerManager = CustomerManager.CreateAsSingleton();
+            customerManager.Save();
+        }
+    }
+
+    class CustomerManager
+    {
+        private CustomerManager() { }
+        private static CustomerManager _customerManager;
+
+        static object _lockObject = new object();
+
+        public static CustomerManager CreateAsSingleton()
+        {
+            lock (_lockObject) // Thread Safe Singleton..
+            {
+                if (_customerManager == null)
+                {
+                    _customerManager = new CustomerManager();
+                }
+            }
+            return _customerManager;
+        }
+
+        public void Save()
+        {
+            Console.WriteLine("Saved!");
+        }
+    }
+}
