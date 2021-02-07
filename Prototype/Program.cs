c18a86c53a3d4eb289492c36cc0581593505156b
ugurkryl41 <ugurkarayel41@gmail.com>
2021-02-07 13:25:42 +0300

    Prototype Design Pattern

---
+++ Prototype/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Prototype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer { Id=1, FirstName="Uğur", LastName="Karayel", City="Kocaeli" };
+
+            Customer customer2 = (Customer)customer1.Clone();
+            customer2.FirstName = "Burcu";
+
+            Console.WriteLine(customer1.FirstName);
+            Console.WriteLine(customer2.FirstName);
+        }
+    }
+
+
+    public abstract class Person
+    {
+        public abstract Person Clone();
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    public class Customer : Person
+    {
+        public string City { get; set; }
+
+        public override Person Clone()
+        {
+            return (Person)MemberwiseClone();
+        }
+    }
+    public class Employee : Person
+    {
+        public string Salary { get; set; }
+
+        public override Person Clone()
+        {
+            return (Person)MemberwiseClone();
+        }
+    }
+}
