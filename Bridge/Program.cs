039c972b0d310d4494cddd54f7a14470b87eb5d8
ugurkryl41 <ugurkarayel41@gmail.com>
2021-02-07 22:34:24 +0300

    Bridge Design Pattern

---
+++ Bridge/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Bridge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.MessageSenderBase = new EMailSender(); 
+            customerManager.UpdateCustomer();
+        }
+    }
+    
+    abstract class MessageSenderBase
+    {
+        public void Save()
+        {
+            Console.WriteLine("Message saved!");
+        }
+
+        public abstract void Send(Body body);
+    }
+
+    class Body
+    {
+        public string Title { get; set; }
+        public string Text { get; set; }
+    }
+
+    class EMailSender : MessageSenderBase
+    {
+        public override void Send(Body body)
+        {
+            Console.WriteLine("{0} was sent via EMailSender",body.Title);
+        }
+    }
+    class SmsSender : MessageSenderBase
+    {
+        public override void Send(Body body)
+        {
+            Console.WriteLine("{0} was sent via SmsSender",body.Title);
+        }
+    }
+
+    class CustomerManager
+    {
+        public MessageSenderBase MessageSenderBase { get; set; }
+
+        public void UpdateCustomer()
+        {
+            Console.WriteLine("Customer updated");
+            MessageSenderBase.Send(new Body { Title="Test" });
+        }
+    }
+}
