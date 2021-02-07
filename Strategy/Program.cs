0f7292ec82535806e196354c62feff85261f206d
ugurkryl41 <ugurkarayel41@gmail.com>
2021-02-07 23:50:40 +0300

    Strategy Design Pattern

---
+++ Strategy/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Strategy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.creditCalculaterBase = new OldCustomer();
+            customerManager.SaveCredit();
+
+            customerManager.creditCalculaterBase = new NewCustomer();
+            customerManager.SaveCredit();
+        }
+    }
+
+    public abstract class CreditCalculaterBase
+    {
+        public abstract decimal Calculate(decimal kredi);
+    }
+
+    class OldCustomer : CreditCalculaterBase
+    {
+        public override decimal Calculate(decimal kredi)
+        {
+            return kredi + kredi * 1.25m;
+        }
+    }
+    class NewCustomer : CreditCalculaterBase
+    {
+        public override decimal Calculate(decimal kredi)
+        {
+            return kredi + kredi * 1.80m;
+        }
+    }
+
+    public class CustomerManager
+    {
+        public CreditCalculaterBase creditCalculaterBase { get; set; }
+        public void SaveCredit()
+        {
+            Console.WriteLine("Customer manager Business");            
+            Console.WriteLine(creditCalculaterBase.Calculate(10000));
+        }
+    }
+
+}
