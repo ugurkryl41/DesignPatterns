fb2bee948350b504dad2f782245544e4472eeed8
ugurkryl41 <ugurkarayel41@gmail.com>
2021-02-09 22:29:27 +0300

    Command Design Pattern

---
+++ Command/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Command
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StockManager stockManager = new StockManager();
+            BuyStock buyStock = new BuyStock(stockManager);
+            SellStock sellStock = new SellStock(stockManager);
+
+            StockController stockController = new StockController();
+            stockController.TakeOrder(buyStock);
+            stockController.TakeOrder(sellStock);
+            stockController.TakeOrder(buyStock);
+
+            stockController.PlaceOrder();
+        }
+    }
+
+    class StockManager
+    {
+        private string _name = "Laptop";
+        private int _quantity = 10;
+
+        public void Buy()
+        {
+            _quantity++;
+            Console.WriteLine("Stock : {0}, {1} bought",_name,_quantity);
+        }
+        public void Sell()
+        {
+            _quantity--;
+            Console.WriteLine("Stock : {0}, {1} sold", _name, _quantity);
+        }
+    }
+
+    interface IOrder // Command Base
+    {
+        void Execute();
+    }
+    class BuyStock : IOrder
+    {
+        private StockManager _stockManager;
+
+        public BuyStock(StockManager stockManager)
+        {
+            _stockManager = stockManager;
+        }
+
+        public void Execute()
+        {
+            _stockManager.Buy();
+        }
+    }
+    class SellStock : IOrder
+    {
+        private StockManager _stockManager;
+
+        public SellStock(StockManager stockManager)
+        {
+            _stockManager = stockManager;
+        }
+
+        public void Execute()
+        {
+            _stockManager.Sell();
+        }
+    }
+
+    class StockController
+    {
+        List<IOrder> _orders = new List<IOrder>();
+
+        public void TakeOrder(IOrder order)
+        {
+            _orders.Add(order);
+        }
+
+        public void PlaceOrder()
+        {
+            foreach (var order in _orders)
+            {
+                order.Execute();
+            }
+            _orders.Clear();
+        }
+    }
+}
