0604a8448c7f6f49af5e0c206e34a17ef8ab1aa1
ugurkryl41 <ugurkarayel41@gmail.com>
2021-02-10 01:03:35 +0300

    Dependency Injection Design Pattern

---
+++ DependencyInjection/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace DependencyInjection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager productManager = new ProductManager(new NhProductDal());
+            productManager.Save();
+        }
+    }
+
+    interface IProductDal
+    {
+        void Save();
+    }
+
+    class EfProductDal:IProductDal
+    {
+        public void Save()
+        {
+            Console.WriteLine("Saved with Ef");
+        }
+    }
+
+    class NhProductDal : IProductDal
+    {
+        public void Save()
+        {
+            Console.WriteLine("Saved with Nh");
+        }
+    }
+
+    class ProductManager
+    {
+        private IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public void Save()
+        {
+            //Business code
+            _productDal.Save();
+        }
+    }
+}
