51c3e218d0443a5721702bac77f7937255c6e89f
ugurkryl41 <ugurkarayel41@gmail.com>
2021-02-09 23:02:09 +0300

    Visitor Design Pattern

---
+++ Visitor/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Visitor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Manager ugur = new Manager { Name = "Uğur", Salary = 2000 };
+            Manager yusuf = new Manager { Name = "yusuf", Salary = 2000 };
+
+            Worker leyla = new Worker { Name = "Leyla", Salary = 1500 };
+            Worker kamil = new Worker { Name = "Kamil", Salary = 1500 };
+
+            ugur.subordi.Add(yusuf);
+            ugur.subordi.Add(leyla);
+            yusuf.subordi.Add(kamil);
+
+            OrganisationalStructure org = new OrganisationalStructure(ugur);
+            PayrollVisitor payrollVisitor = new PayrollVisitor();
+            PayriseVisitor payriseVisitor = new PayriseVisitor(); 
+
+            org.Accept(payrollVisitor);
+            org.Accept(payriseVisitor);
+        }
+    }
+
+    class OrganisationalStructure
+    {
+        public EmployeeBase Employee;
+
+        public OrganisationalStructure(EmployeeBase firstemployee)
+        {
+            Employee = firstemployee;
+        }
+
+        public void Accept(VisitorBase visitor)
+        {
+            Employee.Accept(visitor);
+        }
+    }
+
+    abstract class EmployeeBase
+    {
+        public abstract void Accept(VisitorBase visitor);
+        public string Name { get; set; }
+        public decimal Salary { get; set; }
+    }
+
+    class Manager : EmployeeBase
+    {
+        public Manager()
+        {
+            subordi = new List<EmployeeBase>();
+        }
+        public List<EmployeeBase> subordi { get; set; }
+        public override void Accept(VisitorBase visitor)
+        {
+            visitor.Visit(this);
+            foreach (var employee in subordi)
+            {
+                employee.Accept(visitor);
+            }
+        }
+    }
+    class Worker : EmployeeBase
+    {
+        public override void Accept(VisitorBase visitor)
+        {
+            visitor.Visit(this);
+        }
+    }
+
+    abstract class VisitorBase
+    {
+        public abstract void Visit(Worker worker);
+        public abstract void Visit(Manager manager);
+    }
+
+    class PayrollVisitor : VisitorBase
+    {
+        public override void Visit(Worker worker)
+        {
+            Console.WriteLine("{0} paid {1}",worker.Name,worker.Salary);
+        }
+
+        public override void Visit(Manager manager)
+        {
+            Console.WriteLine("{0} paid {1}", manager.Name, manager.Salary);
+        }
+    }
+
+    class PayriseVisitor : VisitorBase
+    {
+        public override void Visit(Worker worker)
+        {
+            Console.WriteLine("{0} salary increased to {1}", worker.Name, worker.Salary*(decimal)1.1);
+        }
+
+        public override void Visit(Manager manager)
+        {
+            Console.WriteLine("{0} salary increased to {1}", manager.Name, manager.Salary*(decimal)1.2);
+        }
+    }
+}
